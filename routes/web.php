c16b2565d96a9eca610a004082a9cee49b20f053
Shokhrukh2606 <shakhrukh7ielts@mail.ru>
2019-04-14 00:11:45 +0500

    Lab finished

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,45 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('blog.index');
 });
+Route::get('/post/{id}', function ($id) {
+	return view('blog.post', ['id' => $id]);
+})->name('post');
+Route::get('admin/', function () {
+	return view('admin.index');
+})->name('admin.index');
+Route::post('create', function (\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
+	$validation = $validator->make($request->all(), [
+		'title' => 'required|min:5',
+		'content' => 'required|min:10',
+	]);
+	if ($validation->fails()) {
+		return redirect()->back()->withErrors($validation);
+	}
+	return redirect()->route('admin.index')->with('info', 'Post created, Title: ' . $request->input('title'));
+
+})->name('admin.create.new');
+Route::get('admin/create', function () {
+	return view('admin.create');
+})->name('admin.create');
+Route::get('admin/edit/{id}', function ($id) {
+	return view('admin.edit');
+})->name('admin.edit');
+Route::get('/about', function () {
+	return view('other.about');
+});
+Route::post('edit', function (\Illuminate\Http\Request $request,
+	\Illuminate\Validation\Factory $validator) {
+	$validation = $validator->make($request->all(), [
+		'title' => 'required | min :5',
+		'content' => 'required | min :10',
+	]);
+	if ($validation->fails()) {
+		return redirect()->back()->withErrors($validation);
+	}
+	return redirect()->route('admin.index')->with('info', 'Post edited, new Title:' . $request->input('title'));
+})->name('admin.update');
